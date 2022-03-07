d00fb769edb85018a3f6b8cacac911d0bed57b6a
Danielly Garcia Jardim <danielly.jardim@softfocus.com.br>
2022-03-07 20:15:07 -0300

    primeiro commit

---
+++ src/main/java/br/com/fiap/abctechservice/AbcTechServiceApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.abctechservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbcTechServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AbcTechServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fiap/abctechservice/AbcTechServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.abctechservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AbcTechServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
