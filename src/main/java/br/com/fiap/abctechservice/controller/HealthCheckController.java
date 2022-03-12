a62d28196bb4b4ab88fe59f6c297fa257d1e0967
Danielly Garcia Jardim <danielly.jardim@softfocus.com.br>
2022-03-12 14:53:25 -0300

    exibindo version

--- src/main/java/br/com/fiap/abctechservice/controller/HealthCheckController.java
+++ src/main/java/br/com/fiap/abctechservice/controller/HealthCheckController.java
@@ -5,12 +5,29 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
 @RestController
 @RequestMapping("/")
 public class HealthCheckController {
 
     @GetMapping
     public ResponseEntity<String> status() {
-        return ResponseEntity.ok("Sucesso!");
+        return ResponseEntity.ok("Success!");
+    }
+
+    @GetMapping("version")
+    public ResponseEntity<String> version() {
+        Properties properties = new Properties();
+        InputStream inputStream =  getClass().getClassLoader().getResourceAsStream("application.yml");
+        try {
+            properties.load(inputStream);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return ResponseEntity.ok(properties.getProperty("build.name") + " - " + properties.getProperty("build.version"));
     }
 }
